186aa49936f489a22bfb7ff1db787c3f8a316e4e
zinabadelhashem@gmail.com <zinabadelhashem@gmail.com>
2021-05-26 18:20:09 +0200

    Login

--- src/app/MaterialDesign/material/material.module.ts
+++ src/app/MaterialDesign/material/material.module.ts
@@ -3,15 +3,20 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
 import {  MatFormFieldModule } from '@angular/material/form-field';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+
 const materials:any =[
   MatButtonModule,MatToolbarModule,MatSidenavModule,
   MatIconModule,MatCardModule,MatFormFieldModule ,
   MatInputModule,
-  MatTabsModule
+  MatTabsModule,
+  MatCardModule,
+  MatTabsModule,
+  MatInputModule,
+  MatButtonModule
 ];
 @NgModule({
   declarations: [],
