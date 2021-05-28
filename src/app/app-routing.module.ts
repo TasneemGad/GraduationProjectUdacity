ff946149ad7034df5ed04b7bad19d65f81f4b7dc
TasneemGad <tasneemgad38@gmail.com>
2021-05-29 00:23:50 +0200

    footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './Component/HomePage/home-page/home-page.component';
 import { SignUpComponent } from './Component/User/sign-up/sign-up.component';
 
 const routes: Routes = [
+  {path:'Home', component: HomePageComponent},
+
   {path:'SignUP', component: SignUpComponent}
 ];
 
