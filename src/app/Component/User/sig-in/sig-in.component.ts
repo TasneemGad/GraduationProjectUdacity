4331e7a8e4d8d015ded8306bec3c240c490bf2be
zinabadelhashem@gmail.com <zinabadelhashem@gmail.com>
2021-05-29 00:34:56 +0200

    edit

--- src/app/Component/User/sig-in/sig-in.component.ts
+++ src/app/Component/User/sig-in/sig-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-sig-in',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sig-in.component.scss']
 })
 export class SigINComponent implements OnInit {
+  RegisterForm:FormGroup
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+
+  getErrorMessage() {
+    if (this.RegisterForm.get('Name')?.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.RegisterForm.get('Name')?.hasError('Name') ? 'Not a valid Name' : '';
+  }
 
   ngOnInit(): void {
+    this.RegisterForm = this.fb.group({
+      Name: ['', [Validators.required, Validators.maxLength(15)]],
+      Password: ['', [Validators.required, Validators.minLength(6)]],
+      ConfirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+  get Name() {
+    return this.RegisterForm.get('Name');
   }
 
 }
